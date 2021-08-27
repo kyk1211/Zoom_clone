66acb6cd76e8e5f516e21ce8b5a1de6d35b61b5a
곽윤구 <dbsrndi123@naver.com>
2021-08-27 22:47:48 +0900

    making chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,30 @@
+const msgList = document.querySelector('ul');
+const msgForm = document.querySelector('form');
 const frontSocket = new WebSocket(`ws://${window.location.host}`);
+
+function handleOpen() {
+  console.log("Connected to Server ✅");
+}
+
+function handleClose() {
+  console.log("Disconnected from Server ❌");
+}
+
+function handleMsg(msg) {
+  console.log("New message: ", msg.data);
+}
+
+frontSocket.addEventListener("open", handleOpen);
+
+frontSocket.addEventListener("message", handleMsg);
+
+frontSocket.addEventListener("close", handleClose);
+
+function handleSumbit(e) {
+  e.preventDefault();
+  const input = msgForm.querySelector("input");
+  frontSocket.send(input.value);
+  input.value = "";
+}
+
+msgForm.addEventListener('submit', handleSumbit);
--- src/server.js
+++ src/server.js
@@ -7,18 +7,28 @@ const app = express();
 app.set('view engine', "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + '/public'));
-app.get("/", (req, res) => res.render("home"));
-app.get('/*', (req, res) => res.redirect('/'));
+app.get("/", (_, res) => res.render("home"));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const handleConnection = (backSocket) => {
-  console.log(backSocket);
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
 }
 
-wss.on('connection', handleConnection);
+const sockets = [];
+
+wss.on('connection', (backSocket) => {
+  sockets.push(backSocket);
+  console.log("Connected to Browser ✅");
+  backSocket.on("close", onSocketClose);
+  backSocket.on("message", (msg, isBinary) => {
+    const msgString = isBinary ? msg.data : msg.toString();
+    sockets.forEach((aSocket) => aSocket.send(msgString));
+  });
+});
 
 server.listen(3000, handleListen);
