43e0480aed1d12c3e2787ce44576dd9fb152d061
곽윤구 <dbsrndi123@naver.com>
2021-08-28 13:06:36 +0900

    use socketIo start

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
 import express from "express";
-import WebSocket from "ws";
+import SocketIo from "socket.io";
 
 const app = express();
 
@@ -10,36 +10,39 @@ app.use("/public", express.static(__dirname + '/public'));
 app.get("/", (_, res) => res.render("home"));
 app.get('/*', (_, res) => res.redirect('/'));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = SocketIo(server);
+
+
 
 function onSocketClose() {
   console.log("Disconnected from the Browser ❌");
 }
 
-const sockets = [];
-
-wss.on('connection', (backSocket) => {
-  sockets.push(backSocket);
-  backSocket["nickname"] = "Anonymous";
-  console.log("Connected to Browser ✅");
-  backSocket.on("close", onSocketClose);
-  backSocket.on("message", (msg) => {
-    const msgObj = JSON.parse(msg);
-    const msgString = msgObj.payload;
-    switch(msgObj.type) {
-      case "new_msg":
-        sockets.forEach((aSocket) => 
-          aSocket.send(`${backSocket.nickname}: ${msgString}`)
-        );
-        break;
-      case "nickname":
-        backSocket["nickname"] = msgString;
-        break;
-    }
-  });
-});
-
+/* WebSocket */
+// const wss = new WebSocket.Server({ server });
+// const sockets = [];
+// wss.on('connection', (backSocket) => {
+  //   sockets.push(backSocket);
+  //   backSocket["nickname"] = "Anonymous";
+  //   console.log("Connected to Browser ✅");
+  //   backSocket.on("close", onSocketClose);
+  //   backSocket.on("message", (msg) => {
+    //     const msgObj = JSON.parse(msg);
+    //     const msgString = msgObj.payload;
+    //     switch(msgObj.type) {
+      //       case "new_msg":
+      //         sockets.forEach((aSocket) => 
+      //           aSocket.send(`${backSocket.nickname}: ${msgString}`)
+      //         );
+      //         break;
+      //       case "nickname":
+      //         backSocket["nickname"] = msgString;
+      //         break;
+      //     }
+      //   });
+      // });
+      
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 server.listen(3000, handleListen); 
