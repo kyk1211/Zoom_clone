5475fd83cbbb445497a5bfb75680083d0a9c5e59
곽윤구 <dbsrndi123@naver.com>
2021-08-30 21:30:54 +0900

    can use admin-ui

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import express from "express";
-import SocketIo from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -10,8 +11,18 @@ app.use("/public", express.static(__dirname + '/public'));
 app.get("/", (_, res) => res.render("home"));
 app.get('/*', (_, res) => res.redirect('/'));
 
-const server = http.createServer(app);
-const io = SocketIo(server);
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  // demo 작동에 필요한 환경설정
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true
+  }
+});
+
+instrument(io, {
+  auth: false
+});
 
 function publicRooms() {
   const { 
@@ -91,4 +102,4 @@ io.on('connection', (socket) => {
       // });
       
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-server.listen(3000, handleListen); 
+httpServer.listen(3000, handleListen); 
