a8dbf0742259a6d36063ab6cd00fa69f379140ec
곽윤구 <dbsrndi123@naver.com>
2021-08-31 16:31:58 +0900

    can offer

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -10,6 +10,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -98,10 +99,11 @@ const welcomeForm = welcome.querySelector('form');
 
 call.hidden = true;
 
-function startMedia() {
+async function startMedia() {
   welcome.hidden = true;
   call.hidden = false;
-  getMedia();
+  await getMedia();
+  makeConnection();
 }
 
 function handleWelcomeSubmit(e) {
@@ -116,6 +118,24 @@ welcomeForm.addEventListener('submit', handleWelcomeSubmit);
 
 //socket code
 
-socket.on('welcome', () => {
-  console.log('someone joined');
+socket.on('welcome', async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log('sent the offer');
+  socket.emit('offer', offer, roomName);
 });
+
+socket.on('offer', (offer) => {
+  console.log(offer);
+});
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream
+    .getTracks()
+    .forEach((track) => {
+      myPeerConnection.addTrack(track, myStream);
+    });
+}
