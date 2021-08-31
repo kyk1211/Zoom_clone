c429f3238b70f5bf9838d29dbc7f01b3b38033dd
곽윤구 <dbsrndi123@naver.com>
2021-08-31 14:12:02 +0900

    can switch camera

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,14 @@ async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((devices) => (devices.kind === 'videoinput'));
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement('option');
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       cameraSelect.appendChild(option);
     });
   } catch(e) {
@@ -24,14 +28,27 @@ async function getCameras() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstraints = {
+    audio: true,
+    video: { facingMode: 'user' },
+  }
+  const cameraConstraints = {
+    audio: true,
+    video: { 
+      deviceId: { 
+        exact: deviceId 
+      } 
+    },
+  }
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints  
+    );
     myFace.srcObject = myStream;
-    await getCameras();
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -65,5 +82,10 @@ function handleCameraBtn() {
   }
 }
 
+async function handleCameraChange() {
+  await getMedia(cameraSelect.value);
+}
+
 muteBtn.addEventListener('click', handleMuteBtn);
 cameraBtn.addEventListener('click', handleCameraBtn);
+cameraSelect.addEventListener('input', handleCameraChange);
